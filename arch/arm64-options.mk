a1340a441f2a36e5c0c819a6971e6119375bd3fe
magnate3 <liangeaglejun@126.com>
2021-02-19 14:29:18 +0800

    first commit

---
+++ arch/arm64-options.mk
@@ -0,0 +1,19 @@
+# Copyright (c) 2018 Intel Corporation
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# ARM 64 settings
+
+MACHINETYPE := virt
+KERNELPARAMS :=
+MACHINEACCELERATORS :=
+CPUFEATURES := pmu=off
+
+QEMUCMD := qemu-system-aarch64
+
+# Firecracker binary name
+FCCMD := firecracker
+# Firecracker's jailer binary name
+FCJAILERCMD := jailer
+CLHCMD := cloud-hypervisor
---
+++ cli/containerd-shim-kata-v2/main.go
@@ -0,0 +1,30 @@
+// Copyright (c) 2018 HyperHQ Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/containerd/containerd/runtime/v2/shim"
+	"github.com/kata-containers/runtime/containerd-shim-v2"
+)
+
+const shimID = "io.containerd.kata.v2"
+
+func shimConfig(config *shim.Config) {
+	config.NoReaper = true
+	config.NoSubreaper = true
+}
+
+func main() {
+	if len(os.Args) == 2 && os.Args[1] == "--version" {
+		fmt.Printf("%s containerd shim: id: %q, version: %s, commit: %v\n", project, shimID, version, commit)
+		os.Exit(0)
+	}
+
+	shim.Run(shimID, containerdshim.New, shimConfig)
+}
